fc75f439820ca072b660fba9b09cb60adc1bb192
gere149 <gere.delatorre@gmail.com>
2026-02-14 14:32:46 +0100

    ZombiePool

--- Assets/Scripts/ZombiePool.cs
+++ Assets/Scripts/ZombiePool.cs
@@ -1,17 +1,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombiePool : MonoBehaviour 
+public class ZombiePool : MonoBehaviour
 {
-    List<Zombie> m_ZombieList = new List<Zombie>();
-    int m_MaxZombiesOnMap = 30;
+    public static ZombiePool Instance;
 
-    void Update()
+    [SerializeField] private GameObject zombiePrefab;
+    [SerializeField] private int maxZombies = 30;
+
+    private List<GameObject> pool = new List<GameObject>();
+    private int activeZombies = 0;
+
+    void Awake()
     {
-        if (m_ZombieList.Count < m_MaxZombiesOnMap)
-        {
+        Instance = this;
 
+        for (int i = 0; i < maxZombies; i++)
+        {
+            GameObject zombie = Instantiate(zombiePrefab);
+            zombie.SetActive(false);
+            pool.Add(zombie);
         }
     }
 
+    public GameObject GetZombie()
+    {
+        if (activeZombies >= maxZombies)
+            return null;
+
+        foreach (var z in pool)
+        {
+            if (!z.activeInHierarchy)
+            {
+                z.SetActive(true);
+                activeZombies++;
+                return z;
+            }
+        }
+
+        return null;
+    }
+
+    public void ReturnZombie(GameObject zombie)
+    {
+        zombie.SetActive(false);
+        activeZombies--;
+    }
+
+    public int GetActiveCount()
+    {
+        return activeZombies;
+    }
 }
